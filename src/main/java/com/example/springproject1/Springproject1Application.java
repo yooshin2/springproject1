61d9bb21847986664ea6ad9e198753376e65286b
yooshin2 <980513k@dongguk.edu>
2021-08-04 11:28:38 +0900

    Initial commit

---
+++ src/main/java/com/example/springproject1/Springproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.springproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springproject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springproject1/Springproject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
